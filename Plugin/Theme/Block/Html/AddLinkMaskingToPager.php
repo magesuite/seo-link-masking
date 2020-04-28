d131d47a1970182dfdbc685fff1bc1949603ccd1
Paweł Knap <pawel.knap@creativestyle.pl>
2020-04-28 15:49:48 +0200

    - [KRG-1003] implement around plugin for getpagerurl, changing pager permalinks for masked ones if present

---
+++ Plugin/Theme/Block/Html/AddLinkMaskingToPager.php
@@ -0,0 +1,51 @@
+<?php
+namespace MageSuite\SeoLinkMasking\Plugin\Theme\Block\Html;
+
+class AddLinkMaskingToPager
+{
+    const FULL_CATEGORY_ACTION_NAME = 'catalog_category_view';
+
+    /**
+     * @var \MageSuite\SeoLinkMasking\Helper\Configuration
+     */
+    protected $configuration;
+
+    public function __construct(\MageSuite\SeoLinkMasking\Helper\Configuration $configuration)
+    {
+        $this->configuration = $configuration;
+    }
+
+    public function aroundGetPagerUrl(\Magento\Theme\Block\Html\Pager $subject, \Closure $proceed, $params = [])
+    {
+        if (!$this->configuration->isLinkMaskingEnabled()) {
+            return $proceed($params);
+        }
+        return $this->getLinkMaskedPagerUrl($subject, $params);
+    }
+
+    protected function getLinkMaskedPagerUrl(\Magento\Theme\Block\Html\Pager $pager, $params = [])
+    {
+        if ($pager->getRequest()->getFullActionName() != self::FULL_CATEGORY_ACTION_NAME) {
+            return $pager::getPagerUrl($params);
+        }
+
+        $url = $pager->getRequest()->getUriString();
+
+        $fragment = $pager->getFragment();
+        $paginationParam = $pager->getPageVarName();
+        $page = isset($params[$paginationParam]) ? intval($params[$paginationParam]) : null;
+
+        $query = [];
+        parse_str(parse_url($url, PHP_URL_QUERY), $query);
+
+        if ($page === null || $page === 1) {
+            unset($query[$paginationParam]);
+        } else {
+            $query[$paginationParam] = $page;
+        }
+
+        $query = !empty($query) ? '?' . http_build_query($query) : '';
+
+        return strtok($url, '?') . $query . $fragment;
+    }
+}
