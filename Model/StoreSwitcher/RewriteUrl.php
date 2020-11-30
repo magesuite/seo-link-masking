fa81a2c5992ef305ea9d9892345fa232c52d4aa3
Dariusz Twardowski <dariusz.twardowski@creativestyle.pl>
2020-11-30 10:18:39 +0000

    KRG-1289 Language switcher not working when filters are set on category page

---
+++ Model/StoreSwitcher/RewriteUrl.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace MageSuite\SeoLinkMasking\Model\StoreSwitcher;
+
+/**
+ * Handle url rewrites for redirect url with seo link masking paramters
+ */
+class RewriteUrl implements \Magento\Store\Model\StoreSwitcherInterface
+{
+    /**
+     * @var \Magento\UrlRewrite\Model\UrlFinderInterface
+     */
+    private $urlFinder;
+
+    /**
+     * @var \MageSuite\SeoLinkMasking\Service\UrlRewriteFinder
+     */
+    protected $urlRewriteFinder;
+
+    /**
+     * @var \Magento\Framework\HTTP\PhpEnvironment\RequestFactory
+     */
+    private $requestFactory;
+
+    /**
+     * @var \MageSuite\SeoLinkMasking\Model\FilterParametersProcessor
+     */
+    protected $filterParametersProcessor;
+
+    /**
+     * @var \MageSuite\SeoLinkMasking\Helper\Url
+     */
+    protected $urlHelper;
+
+    public function __construct(
+        \Magento\UrlRewrite\Model\UrlFinderInterface $urlFinder,
+        \Magento\Framework\HTTP\PhpEnvironment\RequestFactory $requestFactory,
+        \MageSuite\SeoLinkMasking\Service\UrlRewriteFinder $urlRewriteFinder,
+        \MageSuite\SeoLinkMasking\Model\FilterParametersProcessor $filterParametersProcessor,
+        \MageSuite\SeoLinkMasking\Helper\Url $urlHelper
+    ) {
+        $this->urlFinder = $urlFinder;
+        $this->requestFactory = $requestFactory;
+        $this->urlRewriteFinder = $urlRewriteFinder;
+        $this->filterParametersProcessor = $filterParametersProcessor;
+        $this->urlHelper = $urlHelper;
+    }
+
+    /**
+     * Switch to another store.
+     *
+     * @param \Magento\Store\Api\Data\StoreInterface $fromStore
+     * @param \Magento\Store\Api\Data\StoreInterface $targetStore
+     * @param string $redirectUrl
+     * @return string
+     */
+    public function switch(
+        \Magento\Store\Api\Data\StoreInterface $fromStore,
+        \Magento\Store\Api\Data\StoreInterface $targetStore,
+        string $redirectUrl
+    ): string
+    {
+        $targetUrl = $redirectUrl;
+        /** @var \Magento\Framework\HTTP\PhpEnvironment\Request $request */
+        $request = $this->requestFactory->create(['uri' => $targetUrl]);
+
+        $urlPath = ltrim($request->getPathInfo(), '/');
+
+        if ($targetStore->isUseStoreInUrl()) {
+            // Remove store code in redirect url for correct rewrite search
+            $storeCode = preg_quote($targetStore->getCode() . '/', '/');
+            $pattern = "@^($storeCode)@";
+            $urlPath = preg_replace($pattern, '', $urlPath);
+        }
+
+        $oldStoreId = $fromStore->getId();
+        $oldRewrite = $this->urlRewriteFinder->findRewrite($urlPath, $oldStoreId);
+
+        if ($oldRewrite) {
+            $targetUrl = $targetStore->getBaseUrl();
+            // look for url rewrite match on the target store
+            $currentRewrite = $this->urlFinder->findOneByData([
+                \Magento\UrlRewrite\Service\V1\Data\UrlRewrite::TARGET_PATH => $oldRewrite->getTargetPath(),
+                \Magento\UrlRewrite\Service\V1\Data\UrlRewrite::STORE_ID => $targetStore->getId(),
+            ]);
+            if ($currentRewrite) {
+                $targetUrl .= $currentRewrite->getRequestPath();
+            } else {
+                return $redirectUrl;
+            }
+
+            $filterParams = substr($urlPath, strlen($oldRewrite->getRequestPath()));
+            $processResult = $this->processUrlParameters($filterParams, $oldStoreId, $targetStore->getId());
+            if ($processResult) {
+                $targetUrl .= $processResult;
+            }
+        }
+
+        return $targetUrl;
+    }
+
+    protected function processUrlParameters($params, $oldStoreId, $targetStoreId)
+    {
+        $filterParameters = $this->filterParametersProcessor->processRewrite($params, $oldStoreId, $targetStoreId);
+
+        if(empty($filterParameters)) {
+            return false;
+        }
+
+        return $this->filterParametersProcessor->toUrl($filterParameters);
+    }
+}
---
+++ Test/Integration/Model/StoreSwitcher/RewriteUrlTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Magento\UrlRewrite\Model\StoreSwitcher;
+
+/**
+ * Class RewriteUrlTest
+ * @package Magento\UrlRewrite\Model\StoreSwitcher
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class RewriteUrlTest extends \PHPUnit\Framework\TestCase
+{
+    const CATEGORY_WITH_LINK_MASKING = 778;
+
+    /**
+     * @var \Magento\Store\Model\StoreSwitcher
+     */
+    private $storeSwitcher;
+
+    /**
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    private $objectManager;
+
+    /**
+     * Class dependencies initialization
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        $this->objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+        $this->storeSwitcher = $this->objectManager->get(\Magento\Store\Model\StoreSwitcher::class);
+    }
+
+    /**
+     * @magentoAppArea frontend
+     * @magentoAppIsolation enabled
+     * @magentoDbIsolation disabled
+     * @magentoConfigFixture current_store seo/link_masking/is_short_filter_url_enabled 1
+     * @magentoDataFixture Magento/Store/_files/second_store.php
+     * @magentoDataFixture loadFilterableProductsMultistore
+     * @return void
+     */
+    public function testSwitchToExistingPage(): void
+    {
+        $fromStoreCode = 'default';
+        /** @var \Magento\Store\Api\StoreRepositoryInterface $storeRepository */
+        $storeRepository = $this->objectManager->create(\Magento\Store\Api\StoreRepositoryInterface::class);
+        $fromStore = $storeRepository->get($fromStoreCode);
+
+        $toStoreCode = 'fixture_second_store';
+        /** @var \Magento\Store\Api\StoreRepositoryInterface $storeRepository */
+        $storeRepository = $this->objectManager->create(\Magento\Store\Api\StoreRepositoryInterface::class);
+        $toStore = $storeRepository->get($toStoreCode);
+
+        $redirectUrl = "http://localhost/index.php/test-category/option+1";
+        $expectedUrl = "http://localhost/index.php/test-category-translated/option+1+translated";
+
+        $this->assertEquals($expectedUrl, $this->storeSwitcher->switch($fromStore, $toStore, $redirectUrl));
+    }
+
+    public static function loadFilterableProductsMultistore()
+    {
+        require __DIR__.'/../../_files/filterable_products_multistore.php';
+    }
+
+    public static function loadFilterableProductsMultistoreRollback()
+    {
+        require __DIR__.'/../../_files/filterable_products_multistore_rollback.php';
+    }
+}
