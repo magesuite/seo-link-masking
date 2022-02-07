2ce2edc1079afb2636b0bc91cbd30e78f30ecb71
Grzegorz Bogusz <grzegorz.bogusz@creativestyle.pl>
2022-02-07 10:21:24 +0000

    feat: IPET-683 possibility to change default space replacement character and...

--- Test/Integration/Controller/RouterTest.php
+++ Test/Integration/Controller/RouterTest.php
@@ -9,6 +9,20 @@ namespace MageSuite\SeoLinkMasking\Test\Integration\Controller;
 class RouterTest extends \Magento\TestFramework\TestCase\AbstractController
 {
     const DEFAULT_OPTION_SEPARATOR = '--';
+    const DEFAULT_STORE_ID = 1;
+
+    /**
+     * @var \Magento\Framework\App\CacheInterface
+     */
+    protected $cache;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $objectManager = \Magento\TestFramework\ObjectManager::getInstance();
+        $this->cache = $objectManager->get(\Magento\Framework\App\CacheInterface::class);
+    }
 
     /**
      * @magentoAppArea frontend
@@ -29,6 +43,60 @@ class RouterTest extends \Magento\TestFramework\TestCase\AbstractController
 
         $this->assertArrayHasKey('multiselect_attribute', $parameters);
         $this->assertEquals('Option 1', $parameters['multiselect_attribute']);
+    }
+
+    /**
+     * @magentoAppArea frontend
+     * @magentoDbIsolation enabled
+     * @magentoAppIsolation enabled
+     * @magentoDataFixture loadFilterableProducts
+     * @magentoConfigFixture current_store seo/link_masking/is_short_filter_url_enabled 1
+     * @magentoConfigFixture default/seo/link_masking/space_replacement_character _
+     */
+    public function testItSetFiltersIfSpaceReplacementCharIsChanged()
+    {
+        $cacheKey = $this->getFilterableAttributeOptionsCacheKey(self::DEFAULT_STORE_ID);
+        $this->cache->remove($cacheKey);
+
+        $this->dispatch('/test-category/option_1');
+
+        $assertContains = method_exists($this, 'assertStringContainsString') ? 'assertStringContainsString' : 'assertContains';
+
+        $this->$assertContains('Multiselect Attribute', $this->getResponse()->getBody());
+
+        $parameters = $this->getRequest()->getParams();
+
+        $this->assertArrayHasKey('multiselect_attribute', $parameters);
+        $this->assertEquals('Option 1', $parameters['multiselect_attribute']);
+
+        $this->cache->remove($cacheKey);
+    }
+
+    /**
+     * @magentoAppArea frontend
+     * @magentoDbIsolation enabled
+     * @magentoAppIsolation enabled
+     * @magentoDataFixture loadFilterableProducts
+     * @magentoConfigFixture current_store seo/link_masking/is_short_filter_url_enabled 1
+     * @magentoConfigFixture default/seo/link_masking/excluded_characters otn
+     */
+    public function testItSetFiltersIfExcludedCharactersAreSet()
+    {
+        $cacheKey = $this->getFilterableAttributeOptionsCacheKey(self::DEFAULT_STORE_ID);
+        $this->cache->remove($cacheKey);
+
+        $this->dispatch('/test-category/pi+1');
+
+        $assertContains = method_exists($this, 'assertStringContainsString') ? 'assertStringContainsString' : 'assertContains';
+
+        $this->$assertContains('Multiselect Attribute', $this->getResponse()->getBody());
+
+        $parameters = $this->getRequest()->getParams();
+
+        $this->assertArrayHasKey('multiselect_attribute', $parameters);
+        $this->assertEquals('Option 1', $parameters['multiselect_attribute']);
+
+        $this->cache->remove($cacheKey);
     }
 
     /**
@@ -61,5 +129,10 @@ class RouterTest extends \Magento\TestFramework\TestCase\AbstractController
     public static function loadFilterableProductsRollback()
     {
         require __DIR__.'/../_files/filterable_products_rollback.php';
+    }
+
+    protected function getFilterableAttributeOptionsCacheKey($storeId = null)
+    {
+        return sprintf(\MageSuite\SeoLinkMasking\Service\FilterableAttributeOptionsProvider::CACHE_TAG, $storeId);
     }
 }
