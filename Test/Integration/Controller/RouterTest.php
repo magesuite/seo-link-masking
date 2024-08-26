05a7fe3913463116e459e0a742d0aeb2c00573ba
Dariusz Matoga <dariusz.matoga@creativestyle.pl>
2024-08-26 10:07:44 +0000

    tests: [IPET-2742] Fix compatibility with tests in customizations

--- Test/Integration/Controller/RouterTest.php
+++ Test/Integration/Controller/RouterTest.php
@@ -8,19 +8,17 @@ namespace MageSuite\SeoLinkMasking\Test\Integration\Controller;
  */
 class RouterTest extends \Magento\TestFramework\TestCase\AbstractController
 {
-    const DEFAULT_OPTION_SEPARATOR = '--';
-    const DEFAULT_STORE_ID = 1;
+    protected const DEFAULT_OPTION_SEPARATOR = '--';
+    protected const DEFAULT_STORE_ID = 1;
 
-    /**
-     * @var \Magento\Framework\App\CacheInterface
-     */
-    protected $cache;
+    protected ?\Magento\Framework\App\CacheInterface $cache;
 
     public function setUp(): void
     {
         parent::setUp();
 
         $objectManager = \Magento\TestFramework\ObjectManager::getInstance();
+
         $this->cache = $objectManager->get(\Magento\Framework\App\CacheInterface::class);
     }
 
@@ -31,18 +29,22 @@ class RouterTest extends \Magento\TestFramework\TestCase\AbstractController
      * @magentoDataFixture loadFilterableProducts
      * @magentoConfigFixture current_store seo/link_masking/is_short_filter_url_enabled 1
      */
-    public function testItSetFiltersByValues()
+    public function testItSetFiltersByValues(): void
     {
+        $cacheKey = $this->getFilterableAttributeOptionsCacheKey(self::DEFAULT_STORE_ID);
+        $this->cache->remove($cacheKey);
+
         $this->dispatch('/test-category/option+1');
 
-        $assertContains = method_exists($this, 'assertStringContainsString') ? 'assertStringContainsString' : 'assertContains';
-
+        $assertContains = $this->getAssertContainsMethod();
         $this->$assertContains('Multiselect Attribute', $this->getResponse()->getBody());
 
         $parameters = $this->getRequest()->getParams();
 
         $this->assertArrayHasKey('multiselect_attribute', $parameters);
         $this->assertEquals('Option 1', $parameters['multiselect_attribute']);
+
+        $this->cache->remove($cacheKey);
     }
 
     /**
@@ -53,15 +55,14 @@ class RouterTest extends \Magento\TestFramework\TestCase\AbstractController
      * @magentoConfigFixture current_store seo/link_masking/is_short_filter_url_enabled 1
      * @magentoConfigFixture default/seo/link_masking/space_replacement_character _
      */
-    public function testItSetFiltersIfSpaceReplacementCharIsChanged()
+    public function testItSetFiltersIfSpaceReplacementCharIsChanged(): void
     {
         $cacheKey = $this->getFilterableAttributeOptionsCacheKey(self::DEFAULT_STORE_ID);
         $this->cache->remove($cacheKey);
 
         $this->dispatch('/test-category/option_1');
 
-        $assertContains = method_exists($this, 'assertStringContainsString') ? 'assertStringContainsString' : 'assertContains';
-
+        $assertContains = $this->getAssertContainsMethod();
         $this->$assertContains('Multiselect Attribute', $this->getResponse()->getBody());
 
         $parameters = $this->getRequest()->getParams();
@@ -80,15 +81,14 @@ class RouterTest extends \Magento\TestFramework\TestCase\AbstractController
      * @magentoConfigFixture current_store seo/link_masking/is_short_filter_url_enabled 1
      * @magentoConfigFixture default/seo/link_masking/excluded_characters &
      */
-    public function testItSetFiltersIfExcludedCharactersAreSet()
+    public function testItSetFiltersIfExcludedCharactersAreSet(): void
     {
         $cacheKey = $this->getFilterableAttributeOptionsCacheKey(self::DEFAULT_STORE_ID);
         $this->cache->remove($cacheKey);
 
         $this->dispatch('/test-category/option+with+special+char');
 
-        $assertContains = method_exists($this, 'assertStringContainsString') ? 'assertStringContainsString' : 'assertContains';
-
+        $assertContains = $this->getAssertContainsMethod();
         $this->$assertContains('Multiselect Attribute', $this->getResponse()->getBody());
 
         $parameters = $this->getRequest()->getParams();
@@ -106,12 +106,14 @@ class RouterTest extends \Magento\TestFramework\TestCase\AbstractController
      * @magentoDataFixture loadFilterableProducts
      * @magentoConfigFixture current_store seo/link_masking/is_short_filter_url_enabled 1
      */
-    public function testItSetFilterWithMultipleOptions()
+    public function testItSetFilterWithMultipleOptions(): void
     {
+        $cacheKey = $this->getFilterableAttributeOptionsCacheKey(self::DEFAULT_STORE_ID);
+        $this->cache->remove($cacheKey);
+
         $this->dispatch('/test-category/option+1--option+2');
 
-        $assertContains = method_exists($this, 'assertStringContainsString') ? 'assertStringContainsString' : 'assertContains';
-
+        $assertContains = $this->getAssertContainsMethod();
         $this->$assertContains('Multiselect Attribute', $this->getResponse()->getBody());
 
         $parameters = $this->getRequest()->getParams();
@@ -119,6 +121,8 @@ class RouterTest extends \Magento\TestFramework\TestCase\AbstractController
         $this->assertArrayHasKey('multiselect_attribute', $parameters);
         $this->assertCount(2, $parameters['multiselect_attribute']);
         $this->assertEquals(['Option 1', 'Option 2'], $parameters['multiselect_attribute']);
+
+        $this->cache->remove($cacheKey);
     }
 
     /**
@@ -128,12 +132,14 @@ class RouterTest extends \Magento\TestFramework\TestCase\AbstractController
      * @magentoDataFixture loadFilterableProducts
      * @magentoConfigFixture current_store seo/link_masking/is_short_filter_url_enabled 1
      */
-    public function testItSetFilterWithMultipleOptionsWithSlash()
+    public function testItSetFilterWithMultipleOptionsWithSlash(): void
     {
+        $cacheKey = $this->getFilterableAttributeOptionsCacheKey(self::DEFAULT_STORE_ID);
+        $this->cache->remove($cacheKey);
+
         $this->dispatch('/test-category/option+1/option+2');
 
-        $assertContains = method_exists($this, 'assertStringContainsString') ? 'assertStringContainsString' : 'assertContains';
-
+        $assertContains = $this->getAssertContainsMethod();
         $this->$assertContains('Multiselect Attribute', $this->getResponse()->getBody());
 
         $parameters = $this->getRequest()->getParams();
@@ -141,20 +147,27 @@ class RouterTest extends \Magento\TestFramework\TestCase\AbstractController
         $this->assertArrayHasKey('multiselect_attribute', $parameters);
         $this->assertCount(2, $parameters['multiselect_attribute']);
         $this->assertEquals(['Option 1', 'Option 2'], $parameters['multiselect_attribute']);
+
+        $this->cache->remove($cacheKey);
     }
 
-    public static function loadFilterableProducts()
+    public static function loadFilterableProducts(): void
     {
-        require __DIR__.'/../_files/filterable_products.php';
+        require __DIR__ . '/../_files/filterable_products.php';
     }
 
-    public static function loadFilterableProductsRollback()
+    public static function loadFilterableProductsRollback(): void
     {
-        require __DIR__.'/../_files/filterable_products_rollback.php';
+        require __DIR__ . '/../_files/filterable_products_rollback.php';
     }
 
-    protected function getFilterableAttributeOptionsCacheKey($storeId = null)
+    protected function getFilterableAttributeOptionsCacheKey(?int $storeId = null): string
     {
         return sprintf(\MageSuite\SeoLinkMasking\Service\FilterableAttributeOptionsProvider::CACHE_TAG, $storeId);
+    }
+
+    protected function getAssertContainsMethod(): string
+    {
+        return method_exists($this, 'assertStringContainsString') ? 'assertStringContainsString' : 'assertContains';
     }
 }
