6350fe9fd100b332509e8caae69915fc6a3fa08d
Dariusz Matoga <dariusz.matoga@creativestyle.pl>
2026-01-22 11:38:08 +0100

    chore: Fix PHPUnit deprecations

--- Test/Integration/Model/CategoryTest.php
+++ Test/Integration/Model/CategoryTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace MageSuite\SeoLinkMasking\Test\Integration\Model;
 
 /**
@@ -8,14 +10,11 @@ namespace MageSuite\SeoLinkMasking\Test\Integration\Model;
  */
 class CategoryTest extends \PHPUnit\Framework\TestCase
 {
-    const CATEGORY_WITHOUT_LINK_MASKING = 777;
-    const CATEGORY_WITH_LINK_MASKING = 778;
-    const ROOT_CATEGORY_WITH_LINK_MASKING = 2;
+    protected const CATEGORY_WITHOUT_LINK_MASKING = 777;
+    protected const CATEGORY_WITH_LINK_MASKING = 778;
+    protected const ROOT_CATEGORY_WITH_LINK_MASKING = 2;
 
-    /**
-     * @var \Magento\Catalog\Api\CategoryRepositoryInterface
-     */
-    protected $categoryRepository;
+    protected ?\Magento\Catalog\Api\CategoryRepositoryInterface $categoryRepository;
 
     public function setUp(): void
     {
@@ -29,21 +28,15 @@ class CategoryTest extends \PHPUnit\Framework\TestCase
      * @magentoAppIsolation enabled
      * @magentoDataFixture loadCategories
      * @dataProvider dataProvider
-     * @param integer $categoryId
-     * @param array|null $expectedFilterState
-     * @throws \Magento\Framework\Exception\NoSuchEntityException
      */
-    public function testItReturnsCorrectAttributeValue($categoryId, $expectedFilterState)
+    public function testItReturnsCorrectAttributeValue(int $categoryId, ?array $expectedFilterState): void
     {
         $category = $this->categoryRepository->get($categoryId);
 
         $this->assertEquals($category->getSeoLinkMasking(), $expectedFilterState);
     }
 
-    /**
-     * @return array
-     */
-    public function dataProvider()
+    public static function dataProvider(): array
     {
         return [
             [self::CATEGORY_WITHOUT_LINK_MASKING, null],
@@ -52,12 +45,12 @@ class CategoryTest extends \PHPUnit\Framework\TestCase
         ];
     }
 
-    public static function loadCategories()
+    public static function loadCategories(): void
     {
         require __DIR__ . '/../_files/categories.php';
     }
 
-    public static function loadCategoriesRollback()
+    public static function loadCategoriesRollback(): void
     {
         require __DIR__ . '/../_files/categories_rollback.php';
     }
